799b847d93d1feece959e4a3aced6b29b77afc51
Ben Gardiner <ben.l.gardiner@gmail.com>
2024-07-10 12:33:34 -0400

    Use script-relative paths in Program_ECUA.bat so it can be run from anywhere

--- scripts/STbootloader/windows/ProgramECU_A.bat
+++ scripts/STbootloader/windows/ProgramECU_A.bat
@@ -3,8 +3,8 @@
 SET RAMN_PORT=AUTO
 
 SET STM32PROG_PATH="C:\Program Files\STMicroelectronics\STM32Cube\STM32CubeProgrammer\bin\STM32_Programmer_CLI.exe"
-SET ECU_FIRMWARE_PATH=..\..\firmware\ECUA.hex
+SET ECU_FIRMWARE_PATH=%~dp0\..\..\firmware\ECUA.hex
 
-%STM32PROG_PATH% -c port=usb1 pid=0xdf11 vid=0x0483 -d %ECU_FIRMWARE_PATH% --verify --start 0x08000000 
+%STM32PROG_PATH% -c port=usb1 pid=0xdf11 vid=0x0483 -d "%ECU_FIRMWARE_PATH%" --verify --start 0x08000000
 
 timeout /t 10 > NUL
