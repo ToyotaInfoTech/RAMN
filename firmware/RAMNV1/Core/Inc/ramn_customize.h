cb1ed5ef1debd1d3055bf9005341a109495fcd7d
CamilleTTT <62921765+CamilleTTT@users.noreply.github.com>
2025-02-05 14:02:56 +0900

    Cleaned up customization template

--- firmware/RAMNV1/Core/Inc/ramn_customize.h
+++ firmware/RAMNV1/Core/Inc/ramn_customize.h
@@ -3,7 +3,7 @@
  ******************************************************************************
  * @attention
  *
- * <h2><center>&copy; Copyright (c) 2024 TOYOTA MOTOR CORPORATION.
+ * <h2><center>&copy; Copyright (c) 2025 TOYOTA MOTOR CORPORATION.
  * ALL RIGHTS RESERVED.</center></h2>
  *
  * This software component is licensed by TOYOTA MOTOR CORPORATION under BSD 3-Clause license,
@@ -20,26 +20,26 @@
 
 #include "main.h"
 
-//Function that is called at startup
+// Function that is called at startup
 void 	RAMN_CUSTOM_Init(uint32_t tick);
 
-//Function (from a dedicated task and not an ISR) that is called when a CAN message is received
+// Function (from a dedicated task and not an ISR) that is called when a CAN message is received
 void	RAMN_CUSTOM_ProcessRxCANMessage(const FDCAN_RxHeaderTypeDef* pHeader, const uint8_t* data, uint32_t tick);
 
-//Function that is called periodically
+// Function that is called periodically
 void 	RAMN_CUSTOM_Update(uint32_t tick);
 
 #ifdef ENABLE_I2C
-//Function (in ISR) that is called when data was received on the I2C2 interface
+// Function (in ISR) that is called when data was received on the I2C2 interface
 void RAMN_CUSTOM_ReceiveI2C(uint8_t buf[], uint16_t buf_size);
 
-//Function (in ISR) that is called when data was requested to be transmitted on the I2C2 interface.
+// Function (in ISR) that is called when data was requested to be transmitted on the I2C2 interface.
 void RAMN_CUSTOM_PrepareTransmitDataI2C(uint8_t buf[], uint16_t buf_size);
 #endif
 
 #ifdef ENABLE_UART
 
-//Function called when UART data was received (with \r endline); can be customized for other data formats.
+// Function called when UART data was received (with \r endline); can be customized for other data formats.
 void RAMN_CUSTOM_ReceiveUART(uint8_t buf[], uint16_t buf_size);
 #endif
 
