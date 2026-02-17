47fe8306b520d8d8bf7a2f028bb477cee1c5ed12
Ben Gardiner <ben.l.gardiner@gmail.com>
2026-02-16 22:23:12 -0500

    signals: refactor existing signals to generalize for arbitrary bitfields later

---
+++ firmware/RAMNV1/Core/Inc/ramn_can_database.h
@@ -0,0 +1,59 @@
+#ifndef RAMN_CAN_DATABASE_H
+#define RAMN_CAN_DATABASE_H
+
+#include <stdint.h>
+
+void RAMN_Encode_Command_Brake(uint16_t value, uint8_t* payload);
+uint16_t RAMN_Decode_Command_Brake(const uint8_t* payload, uint32_t dlc);
+
+void RAMN_Encode_Control_Brake(uint16_t value, uint8_t* payload);
+uint16_t RAMN_Decode_Control_Brake(const uint8_t* payload, uint32_t dlc);
+
+void RAMN_Encode_Command_Accel(uint16_t value, uint8_t* payload);
+uint16_t RAMN_Decode_Command_Accel(const uint8_t* payload, uint32_t dlc);
+
+void RAMN_Encode_Control_Accel(uint16_t value, uint8_t* payload);
+uint16_t RAMN_Decode_Control_Accel(const uint8_t* payload, uint32_t dlc);
+
+void RAMN_Encode_Status_RPM(uint16_t value, uint8_t* payload);
+uint16_t RAMN_Decode_Status_RPM(const uint8_t* payload, uint32_t dlc);
+
+void RAMN_Encode_Command_Steering(uint16_t value, uint8_t* payload);
+uint16_t RAMN_Decode_Command_Steering(const uint8_t* payload, uint32_t dlc);
+
+void RAMN_Encode_Control_Steering(uint16_t value, uint8_t* payload);
+uint16_t RAMN_Decode_Control_Steering(const uint8_t* payload, uint32_t dlc);
+
+void RAMN_Encode_Command_Shift(uint8_t value, uint8_t* payload);
+uint8_t RAMN_Decode_Command_Shift(const uint8_t* payload, uint32_t dlc);
+
+void RAMN_Encode_Control_Shift_Joystick(uint8_t shift_value, uint8_t joystick_value, uint8_t* payload);
+uint8_t RAMN_Decode_Control_Shift(const uint8_t* payload, uint32_t dlc);
+
+uint8_t RAMN_Decode_Joystick(const uint8_t* payload, uint32_t dlc);
+
+void RAMN_Encode_Command_Horn(uint8_t value, uint8_t* payload);
+uint8_t RAMN_Decode_Command_Horn(const uint8_t* payload, uint32_t dlc);
+
+void RAMN_Encode_Control_Horn(uint8_t value, uint8_t* payload);
+uint8_t RAMN_Decode_Control_Horn(const uint8_t* payload, uint32_t dlc);
+
+void RAMN_Encode_Command_TurnIndicator(uint16_t value, uint8_t* payload);
+uint16_t RAMN_Decode_Command_TurnIndicator(const uint8_t* payload, uint32_t dlc);
+
+void RAMN_Encode_Command_Sidebrake(uint16_t value, uint8_t* payload);
+uint16_t RAMN_Decode_Command_Sidebrake(const uint8_t* payload, uint32_t dlc);
+
+void RAMN_Encode_Control_Sidebrake(uint8_t value, uint8_t* payload);
+uint8_t RAMN_Decode_Control_Sidebrake(const uint8_t* payload, uint32_t dlc);
+
+void RAMN_Encode_Control_EngineKey(uint8_t value, uint8_t* payload);
+uint8_t RAMN_Decode_Control_EngineKey(const uint8_t* payload, uint32_t dlc);
+
+void RAMN_Encode_Command_Lights(uint16_t value, uint8_t* payload);
+uint16_t RAMN_Decode_Command_Lights(const uint8_t* payload, uint32_t dlc);
+
+void RAMN_Encode_Control_Lights(uint8_t value, uint8_t* payload);
+uint8_t RAMN_Decode_Control_Lights(const uint8_t* payload, uint32_t dlc);
+
+#endif // RAMN_CAN_DATABASE_H
--- firmware/RAMNV1/Core/Src/ramn_actuators.c
+++ firmware/RAMNV1/Core/Src/ramn_actuators.c
@@ -15,6 +15,8 @@
  */
 
 #include "ramn_actuators.h"
+#include "ramn_signal_defs.h"
+#include "ramn_can_database.h"
 
 #ifdef EXPANSION_BODY
 // Byte that store the state of each LED of ECU D.
@@ -44,34 +46,29 @@ void RAMN_ACTUATORS_SetLampState(uint8_t mask, uint8_t val)
 
 void RAMN_ACTUATORS_ApplyControls(uint32_t tick)
 {
-
 #if defined(EXPANSION_CHASSIS) //CHASSIS
-
-	msg_control_steering.data->ramnData.payload = applyEndian16((uint16_t)RAMN_DBC_Handle.control_steer);
-	msg_control_sidebrake.data->ramnData.payload = RAMN_DBC_Handle.control_sidebrake;
-	msg_command_lights.data->ramnData.payload = RAMN_DBC_Handle.command_lights;
+	RAMN_Encode_Control_Steering((uint16_t)RAMN_DBC_Handle.control_steer, &msg_control_steering.data->rawData[CAN_SIM_CONTROL_STEERING_PAYLOAD_OFFSET / 8]);
+	RAMN_Encode_Control_Sidebrake((uint8_t)RAMN_DBC_Handle.control_sidebrake, &msg_control_sidebrake.data->rawData[CAN_SIM_CONTROL_SIDEBRAKE_PAYLOAD_OFFSET / 8]);
+	RAMN_Encode_Command_Lights((uint16_t)RAMN_DBC_Handle.command_lights, &msg_command_lights.data->rawData[CAN_SIM_COMMAND_LIGHTS_PAYLOAD_OFFSET / 8]);
 
 #elif defined(EXPANSION_POWERTRAIN) //POWERTRAIN
-
-	msg_control_brake.data->ramnData.payload = applyEndian16((uint16_t)RAMN_DBC_Handle.control_brake);
-	msg_control_accel.data->ramnData.payload = applyEndian16((uint16_t)RAMN_DBC_Handle.control_accel);
-	msg_control_shift.data->ramnData.payload = RAMN_DBC_Handle.control_shift | (RAMN_DBC_Handle.joystick << 8);
-	msg_command_horn.data->ramnData.payload = RAMN_DBC_Handle.command_horn;
-	msg_command_turnindicator.data->ramnData.payload = RAMN_DBC_Handle.command_turnindicator;
+	RAMN_Encode_Control_Brake((uint16_t)RAMN_DBC_Handle.control_brake, &msg_control_brake.data->rawData[CAN_SIM_CONTROL_BRAKE_PAYLOAD_OFFSET / 8]);
+	RAMN_Encode_Control_Accel((uint16_t)RAMN_DBC_Handle.control_accel, &msg_control_accel.data->rawData[CAN_SIM_CONTROL_ACCEL_PAYLOAD_OFFSET / 8]);
+	RAMN_Encode_Control_Shift_Joystick((uint8_t)RAMN_DBC_Handle.control_shift, (uint8_t)RAMN_DBC_Handle.joystick, &msg_control_shift.data->rawData[CAN_SIM_CONTROL_SHIFT_PAYLOAD_OFFSET / 8]);
+	RAMN_Encode_Control_Horn((uint8_t)RAMN_DBC_Handle.control_horn, &msg_control_horn.data->rawData[CAN_SIM_CONTROL_HORN_PAYLOAD_OFFSET / 8]);
+	RAMN_Encode_Command_TurnIndicator((uint16_t)RAMN_DBC_Handle.command_turnindicator, &msg_command_turnindicator.data->rawData[CAN_SIM_COMMAND_TURNINDICATOR_PAYLOAD_OFFSET / 8]);
 
 #elif defined(EXPANSION_BODY) //BODY
+	RAMN_Encode_Control_EngineKey((uint8_t)RAMN_DBC_Handle.control_enginekey, &msg_control_enginekey.data->rawData[CAN_SIM_CONTROL_ENGINEKEY_PAYLOAD_OFFSET / 8]);
+	RAMN_Encode_Control_Lights((uint8_t)RAMN_DBC_Handle.control_lights, &msg_control_lights.data->rawData[CAN_SIM_CONTROL_LIGHTS_PAYLOAD_OFFSET / 8]);
 
-	msg_control_enginekey.data->ramnData.payload = RAMN_DBC_Handle.control_enginekey;
-	msg_control_lights.data->ramnData.payload = RAMN_DBC_Handle.control_lights;
-
+	LEDState = (uint8_t)RAMN_DBC_Handle.control_lights;
 #if (LED_TEST_DURATION_MS > 0)
-
-	if((tick < LED_TEST_DURATION_MS) && (LEDTestOver == False)) RAMN_DBC_Handle.control_lights = 0xFF;
+	if((tick < LED_TEST_DURATION_MS) && (LEDTestOver == False)) LEDState = 0xFF;
 	else LEDTestOver = True;
-
 #endif
 #ifdef ENABLE_SPI
-	RAMN_SPI_UpdateLED((uint8_t*)&(RAMN_DBC_Handle.control_lights));
+	RAMN_SPI_UpdateLED(&LEDState);
 #endif
 #endif
 }
