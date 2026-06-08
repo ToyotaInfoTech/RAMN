e34ad5fd6be33a27176ee7f1d1a7caeb66302650
CamilleTTT <62921765+CamilleTTT@users.noreply.github.com>
2026-06-09 08:51:56 +0900

    Expect .hex instead of .bin

--- scripts/STbootloader/linux/ProgramECU_A.sh
+++ scripts/STbootloader/linux/ProgramECU_A.sh
@@ -1,6 +1,5 @@
 ECU_FIRMWARE_PATH=../../firmware
 
-dfu-util  -d 0x0483:0xdf11 -c1 -a0 -D "$ECU_FIRMWARE_PATH/ECUA.bin" --dfuse-address 0x08000000:leave	 
-
+dfu-util -d 0x0483:0xdf11 -c1 -a0 -D "$ECU_FIRMWARE_PATH/ECUA.hex" -R
 
 sleep 10
